b9530e517a88e6f5aa7362dcc34bf711caf78619
Javier Jordán Luque <javierjordanluque@gmail.com>
2023-03-20 14:18:41 +0100

    Modified Assistant Controller and created AssistantChats.jsp

---
+++ src/main/java/com/taw/polybank/controller/AssistantController.java
@@ -0,0 +1,32 @@
+package com.taw.polybank.controller;
+
+import com.taw.polybank.dao.ChatRepository;
+import com.taw.polybank.dao.EmployeeRepository;
+import com.taw.polybank.entity.ChatEntity;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("employee/assistant")
+public class AssistantController {
+
+    @Autowired
+    protected EmployeeRepository employeeRepository;
+
+    @Autowired
+    protected ChatRepository chatRepository;
+
+    @GetMapping("/")
+    public String doListChats(Model model, HttpSession session) {
+        List<ChatEntity> chatList = this.chatRepository.findAll();
+        model.addAttribute("chatList", chatList);
+
+        return "assistantChats";
+    }
+}
---
+++ src/main/webapp/WEB-INF/view/assistantChats.jsp
@@ -0,0 +1,43 @@
+<%@ page import="com.taw.polybank.entity.ChatEntity" %>
+<%@ page import="java.util.List" %><%--
+  Created by IntelliJ IDEA.
+  User: Javier Jordán Luque
+  Date: 20/03/2023
+  Time: 13:51
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+
+<%
+    List<ChatEntity> chatList = (List<ChatEntity>) request.getAttribute("chatList");
+%>
+
+<html>
+<head>
+    <title>Polybank - Assistant</title>
+</head>
+<body>
+<h1>Chats List</h1>
+<table border="1">
+    <tr>
+        <th>ID</th>
+        <th>CLIENT</th>
+        <th>CLOSED</th>
+        <th></th>
+    </tr>
+    <%
+        for (ChatEntity chat : chatList) {
+    %>
+    <tr>
+        <td><%= chat.getId() %></td>
+        <td><%= chat.getClientId() %></td>
+        <td><%= chat.getClosed() %></td>
+        <td><a href="/chat/client?id=<%= chat.getId() %>">Open</a></td>
+    </tr>
+
+    <%
+        }
+    %>
+</table border="1">
+</body>
+</html>
