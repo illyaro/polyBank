ba1f15cc3f5acd84c0285efc0774d6f7926cfbf4
José Manuel <jmsanchezrico@gmail.com>
2023-04-02 09:50:20 +0200

    🗃️ Nullable relationship on Transaction

--- src/main/java/com/taw/polybank/entity/TransactionEntity.java
+++ src/main/java/com/taw/polybank/entity/TransactionEntity.java
@@ -15,18 +15,18 @@ public class TransactionEntity {
     @Basic
     @Column(name = "timestamp")
     private Timestamp timestamp;
-    @Basic
-    @Column(name = "Client_id")
-    private int clientId;
-    @Basic
-    @Column(name = "BankAccount_id")
-    private int bankAccountId;
-    @Basic
-    @Column(name = "CurrencyExchange_id")
-    private int currencyExchangeId;
-    @Basic
-    @Column(name = "Payment_id")
-    private int paymentId;
+    @ManyToOne
+    @JoinColumn(name = "Client_id", referencedColumnName = "id", nullable = false)
+    private ClientEntity clientByClientId;
+    @ManyToOne
+    @JoinColumn(name = "BankAccount_id", referencedColumnName = "id", nullable = false)
+    private BankAccountEntity bankAccountByBankAccountId;
+    @ManyToOne
+    @JoinColumn(name = "CurrencyExchange_id", referencedColumnName = "id", nullable = true)
+    private CurrencyExchangeEntity currencyExchangeByCurrencyExchangeId;
+    @ManyToOne
+    @JoinColumn(name = "Payment_id", referencedColumnName = "id", nullable = true)
+    private PaymentEntity paymentByPaymentId;
 
     public int getId() {
         return id;
@@ -44,48 +44,48 @@ public class TransactionEntity {
         this.timestamp = timestamp;
     }
 
-    public int getClientId() {
-        return clientId;
-    }
-
-    public void setClientId(int clientId) {
-        this.clientId = clientId;
-    }
-
-    public int getBankAccountId() {
-        return bankAccountId;
-    }
-
-    public void setBankAccountId(int bankAccountId) {
-        this.bankAccountId = bankAccountId;
-    }
-
-    public int getCurrencyExchangeId() {
-        return currencyExchangeId;
-    }
-
-    public void setCurrencyExchangeId(int currencyExchangeId) {
-        this.currencyExchangeId = currencyExchangeId;
-    }
-
-    public int getPaymentId() {
-        return paymentId;
-    }
-
-    public void setPaymentId(int paymentId) {
-        this.paymentId = paymentId;
-    }
-
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         TransactionEntity that = (TransactionEntity) o;
-        return id == that.id && clientId == that.clientId && bankAccountId == that.bankAccountId && currencyExchangeId == that.currencyExchangeId && paymentId == that.paymentId && Objects.equals(timestamp, that.timestamp);
+        return id == that.id && Objects.equals(timestamp, that.timestamp);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, timestamp, clientId, bankAccountId, currencyExchangeId, paymentId);
+        return Objects.hash(id, timestamp);
+    }
+
+    public ClientEntity getClientByClientId() {
+        return clientByClientId;
+    }
+
+    public void setClientByClientId(ClientEntity clientByClientId) {
+        this.clientByClientId = clientByClientId;
+    }
+
+    public BankAccountEntity getBankAccountByBankAccountId() {
+        return bankAccountByBankAccountId;
+    }
+
+    public void setBankAccountByBankAccountId(BankAccountEntity bankAccountByBankAccountId) {
+        this.bankAccountByBankAccountId = bankAccountByBankAccountId;
+    }
+
+    public CurrencyExchangeEntity getCurrencyExchangeByCurrencyExchangeId() {
+        return currencyExchangeByCurrencyExchangeId;
+    }
+
+    public void setCurrencyExchangeByCurrencyExchangeId(CurrencyExchangeEntity currencyExchangeByCurrencyExchangeId) {
+        this.currencyExchangeByCurrencyExchangeId = currencyExchangeByCurrencyExchangeId;
+    }
+
+    public PaymentEntity getPaymentByPaymentId() {
+        return paymentByPaymentId;
+    }
+
+    public void setPaymentByPaymentId(PaymentEntity paymentByPaymentId) {
+        this.paymentByPaymentId = paymentByPaymentId;
     }
 }
