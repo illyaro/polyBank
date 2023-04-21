11f395fd246be72d588f87e0b93c379dc1cc1a71
José Manuel <jmsanchezrico@gmail.com>
2023-04-21 09:14:16 +0200

    Repository fix DTO to Entity

--- src/main/java/com/taw/polybank/dao/TransactionRepository.java
+++ src/main/java/com/taw/polybank/dao/TransactionRepository.java
@@ -14,13 +14,13 @@ import java.util.List;
 public interface TransactionRepository extends JpaRepository<TransactionEntity, Integer> {
     public List<TransactionEntity> findByBankAccountByBankAccountId(BankAccountEntity bankAccount);
 
-    @Query("select t from TransactionDTO t where t.bankAccountByBankAccountId = :bankAccount and t.timestamp >= :begin and t.timestamp <= :end and t.clientByClientId in :clients and t.paymentByPaymentId.benficiaryByBenficiaryId = :beneficiary and t.paymentByPaymentId.amount >= :amount")
+    @Query("select t from TransactionEntity t where t.bankAccountByBankAccountId = :bankAccount and t.timestamp >= :begin and t.timestamp <= :end and t.clientByClientId in :clients and t.paymentByPaymentId.benficiaryByBenficiaryId = :beneficiary and t.paymentByPaymentId.amount >= :amount")
     List<TransactionEntity> filterByBankAccount_TimestampRange_TransactionOwner_beneficiaryIBAN_Amount(BankAccountEntity bankAccount, Timestamp begin, Timestamp end, List<ClientEntity> clients, BenficiaryEntity beneficiary, double amount);
 
-    @Query("select t from TransactionDTO t where t.bankAccountByBankAccountId = :bankAccount and t.timestamp >= :begin and t.timestamp <= :end and t.paymentByPaymentId.benficiaryByBenficiaryId = :beneficiary and t.paymentByPaymentId.amount >= :amount")
+    @Query("select t from TransactionEntity t where t.bankAccountByBankAccountId = :bankAccount and t.timestamp >= :begin and t.timestamp <= :end and t.paymentByPaymentId.benficiaryByBenficiaryId = :beneficiary and t.paymentByPaymentId.amount >= :amount")
     List<TransactionEntity> filterByBankAccount_TimestampRange_beneficiaryIBAN_Amount(BankAccountEntity bankAccount, Timestamp begin, Timestamp end, BenficiaryEntity beneficiary, double amount);
 
-    @Query("select t from TransactionDTO t where t.bankAccountByBankAccountId = :bankAccount and t.timestamp >= :begin and t.timestamp <= :end and t.clientByClientId in :clients and t.paymentByPaymentId.amount >= :amount")
+    @Query("select t from TransactionEntity t where t.bankAccountByBankAccountId = :bankAccount and t.timestamp >= :begin and t.timestamp <= :end and t.clientByClientId in :clients and t.paymentByPaymentId.amount >= :amount")
     List<TransactionEntity> filterByBankAccount_TimestampRange_TransactionOwner_Amount(BankAccountEntity bankAccount, Timestamp begin, Timestamp end, List<ClientEntity> clients, double amount);
 
     @Query("select t from TransactionDTO t where t.bankAccountByBankAccountId = :bankAccount and t.timestamp >= :begin and t.timestamp <= :end and t.paymentByPaymentId.amount >= :amount")
