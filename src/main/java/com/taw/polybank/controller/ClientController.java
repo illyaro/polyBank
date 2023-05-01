e0e781d9d907c9b19d04a102c3f68167f1995d40
firehellrain <firehellrain@gmail.com>
2023-05-01 17:47:35 +0200

    Added moneyExchange view
    Added the view of the money exchange option inside the Client's bank account.
    
    TODO: Add the functionality of the money exchange

--- src/main/java/com/taw/polybank/controller/ClientController.java
+++ src/main/java/com/taw/polybank/controller/ClientController.java
@@ -1,16 +1,20 @@
 package com.taw.polybank.controller;
 
+import com.taw.polybank.controller.company.Client;
+import com.taw.polybank.dao.BadgeRepository;
 import com.taw.polybank.dao.BankAccountRepository;
 import com.taw.polybank.dao.ClientRepository;
 import com.taw.polybank.dto.ClientDTO;
-import com.taw.polybank.entity.BankAccountEntity;
-import com.taw.polybank.entity.ClientEntity;
+import com.taw.polybank.entity.*;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
+import java.util.List;
 
 @Controller
 @RequestMapping("/client")
@@ -19,6 +23,8 @@ public class ClientController {
     private ClientRepository clientRepository;
     @Autowired
     private BankAccountRepository bankAccountRepository;
+    @Autowired
+    private BadgeRepository badgeRepository;
 
     @GetMapping("/view")
     public String viewClient(Model model, HttpSession session) {
@@ -66,6 +72,60 @@ public class ClientController {
         }
     }
 
+    @GetMapping("/account/moneyExchange")
+    public String moneyExchangeOnBankAccount (@RequestParam("id") Integer accountID, Model model, HttpSession session) {
+        BadgeEntity badge = new BadgeEntity();
+        List<BadgeEntity> badgeList = this.badgeRepository.findAll();
+        BankAccountEntity account = this.bankAccountRepository.findById(accountID).orElse(null);
+        model.addAttribute("badge", badge);
+        model.addAttribute("account", account);
+        model.addAttribute("badgeList", badgeList);
+        return "client/bankAccount/moneyExchange";
+    }
+/*
+    @PostMapping("/makeExchange")
+    public String makeExchange(@ModelAttribute BadgeEntity targetBadge, HttpSession session, Model model) {
+
+        Integer clientID = (Integer) session.getAttribute("clientID");
+        Client client = this.clientRepository.findById(clientID).orElse(null);
+        BankAccountEntity bankAccount = company.getBankAccountByBankAccountId();
+        BadgeEntity currentBadge = bankAccount.getBadgeByBadgeId();
+        targetBadge = badgeRepository.findById(targetBadge.getId()).get();
+        TransactionEntity transaction = defineTransaction(client, bankAccount);
+
+        BenficiaryEntity beneficiary = beneficiaryRepository.findBenficiaryEntityByNameAndIban(company.getName(), bankAccount.getIban());
+        PaymentEntity payment = definePayment(bankAccount.getBalance(), beneficiary, transaction);
+
+        if (beneficiary == null) {
+            beneficiary = defineBeneficiary(company.getName(), bankAccount.getIban(), targetBadge, payment);
+        } else {
+            Collection<PaymentEntity> allPayments = beneficiary.getPaymentsById();
+            allPayments.add(payment);
+            beneficiary.setPaymentsById(allPayments);
+        }
+        payment.setBenficiaryByBenficiaryId(beneficiary);
+
+        if (currentBadge.getId() != targetBadge.getId()) {
+            CurrencyExchangeEntity currencyExchange = defineCurrencyExchange(currentBadge, targetBadge, bankAccount.getBalance(), transaction, payment);
+            payment.setCurrencyExchangeByCurrencyExchangeId(currencyExchange);
+            transaction.setCurrencyExchangeByCurrencyExchangeId(currencyExchange);
+            transaction.setPaymentByPaymentId(payment);
+            updateBadges(currentBadge, targetBadge, currencyExchange);
+            currencyExchangeRepository.save(currencyExchange);
+            bankAccount.setBalance(currencyExchange.getFinalAmount());
+            bankAccount.setBadgeByBadgeId(targetBadge);
+            beneficiaryRepository.save(beneficiary);
+            paymentRepository.save(payment);
+            transactionRepository.save(transaction);
+            clientRepository.save(client.getClient());
+            bankAccountRepository.save(bankAccount);
+            model.addAttribute("message", currencyExchange.getInitialAmount() + " " + currentBadge.getName() + " was successfully exchanged to " + currencyExchange.getFinalAmount() + " " + targetBadge.getName());
+        } else {
+            model.addAttribute("message", "No exchange was made, chosen currency is actual currency of your bank account.");
+        }
+        return "/company/userHomepage";
+    }
+*/
     @PostMapping("/login")
     public String postLogin(@RequestParam("dni") String dni, @RequestParam("password") String password,
                             HttpSession session) {
---
+++ src/main/webapp/WEB-INF/view/client/bankAccount/moneyExchange.jsp
@@ -0,0 +1,31 @@
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
+<%@ page import="com.taw.polybank.entity.BankAccountEntity" %><%--
+<%--
+  Created by IntelliJ IDEA.
+  User: Illya Rozumovskyy
+  Date: 07/04/2023
+  Time: 23:34
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Polybank - Bank Account</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+</head>
+<body>
+<%
+    BankAccountEntity account = (BankAccountEntity) request.getAttribute("account");
+%>
+<div id="transactionWindow">
+    <h1>Money Exchange</h1>
+    <p>Balance: <%=account.getBalance()%> <%=account.getBadgeByBadgeId().getName()%></p><br>
+
+    <form:form modelAttribute="badge" method="post" action="/client/account/makeExchange">
+        <form:label path="id">Desired currency: </form:label>
+        <form:select path="id" items="${badgeList}" itemLabel="name" itemValue="id"/><br><br>
+        <form:button class="btn btn-primary">Exchange</form:button>
+    </form:form>
+</div>
+</body>
+</html>
